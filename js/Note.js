91f92303f66447618614cf8daceddebca9a75288
Kb Bishi <icodeclean@gmail.com>
2016-10-10 02:48:08 -0400

    Added-Styling-To-Notes

--- js/Note.js
+++ js/Note.js
@@ -2,6 +2,17 @@ var Note = React.createClass({
 	getInitialState: function() {
 		return {editing: false};
 	},
+	componentWillMount: function() {
+		this.style = {
+			right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+			top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+			transform: 'rotate(' + this.randomBetween(-15,15) + 'deg)'
+		};
+	},
+	randomBetween: function(min, max) {
+		return min + Math.ceil(Math.random() * max);
+	},
+
 	save: function() {
 		this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
 		this.setState({editing: false});
@@ -15,7 +26,7 @@ var Note = React.createClass({
 
 	renderForm: function() {
 		return (
-			<div className ="note">
+			<div className ="note" style={this.style} >
 				<textarea className="form-control" ref="newText" defaultValue={this.props.children}>
 				</textarea>
 				<button onClick={this.save} className="btn btn-success glyphicon glyphicon-check"/>
@@ -25,7 +36,7 @@ var Note = React.createClass({
 
 	renderDisplay: function() {
 		return (
-			<div className ="note">
+			<div className ="note" style={this.style}>
 				<p>{this.props.children}</p>
 				<span>
 					<button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-edit"/>
